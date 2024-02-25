d2b795d47c88ccaa9dc21b29885e87f125c48fbc
Username <dernur0505daryn@gmail.com>
2024-02-25 15:33:31 +0600

    cors

--- server.js
+++ server.js
@@ -28,22 +28,10 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-const allowedOrigins = [
-    'https://node-final.ems-app.kz'
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified origin.' + origin;
-            return callback(new Error(msg), false);
-        }
-
-        return callback(null, true);
-    },
+    origin: '*',
 }));
+
 
 app.use(express.json())
 app.use('/node-api/admin', AdminRoutes)
