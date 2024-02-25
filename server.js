a96afd9c24b14e8670e9ed0a2f41bc8ca0c9b228
Username <dernur0505daryn@gmail.com>
2024-02-25 15:09:40 +0600

    SSL

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ const CourseRoutes = require('./src/routes/CourseRouter.js')
 const sequelize = require('./src/utils/db.js')
 const mongo = require('./src/utils/mongodb.js')
 const cors = require('cors')
+const https = require("https");
 const port = 8000
 
 const app = express()
+const server = https.createServer(
+    {
+        key: fs.readFileSync(
+            "/etc/letsencrypt/archive/ems-app.kz/privkey1.pem",
+            "utf8"
+        ),
+        cert: fs.readFileSync(
+            "/etc/letsencrypt/archive/ems-app.kz/cert1.pem",
+            "utf8"
+        ),
+    },
+    app
+);
 
 app.get('/', (req, res) => {
     res.send('Hello')
@@ -25,4 +39,4 @@ app.use('/node-api/course', CourseRoutes)
 //     console.log('Tables have been created')
 // })()
 
-app.listen(port, () => console.log('App started'))
+server.listen(port, () => console.log('App started'))
